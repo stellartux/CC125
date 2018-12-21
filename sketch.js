ffceeea73e3a02e6ee8a2543e7667be5dd9ab3ab
Danny Robinson <dannyrobinson@gmx.com>
2018-12-21 17:25:28 +0000

    Added presets and phase inversion

--- sketch.js
+++ sketch.js
@@ -9,7 +9,6 @@ let wave = []
 let sliderlist
 const ctx = new (window.AudioContext || window.webkitAudioContext)()
 const gainNode = ctx.createGain()
-gainNode.gain.value = 0.125
 gainNode.connect(ctx.destination)
 
 let osc
@@ -20,26 +19,62 @@ function addSliderComponent () {
       document.getElementById('slider-template').content, true))
 }
 
+function setSliderCount (val) {
+  if (val <= 0) return
+  while (sliderlist.childElementCount < val) {
+    addSliderComponent()
+  }
+  while (sliderlist.childElementCount > val) {
+    sliderlist.removeChild(sliderlist.lastChild)
+  }
+}
+
+function setSlidersToPreset (preset, phases) {
+  setSliderCount(preset.length)
+  document.getElementById('slider-count').value = preset.length
+  for (let i = 0; i < sliderlist.childElementCount; i++) {
+    const el = sliderlist.children[i].getElementsByClassName('radius')[0]
+    el.value = preset[i] * el.getAttribute('max')
+    sliderlist.children[i].getElementsByClassName('phase')[0].checked = phases && phases[i]
+  }
+}
+
 function setup () {
   createCanvas(600, 400)
   sliderlist = document.getElementById('sliders')
   for (let i = 0; i < document.getElementById('slider-count').value; i++) {
     addSliderComponent()
   }
-
   document.getElementById('slider-count').onchange = e => {
-    const val = e.target.value | 0
-    while (sliderlist.childElementCount < val) {
-      addSliderComponent()
+    setSliderCount(e.target.value | 0)
+  }
+  document.getElementById('square-wave').onclick = e => {
+    let preset = []
+    for (let n = 1; n <= 13; n++) {
+      preset.push(n % 2 ? (4 / (Math.PI * n)) / (4 / Math.PI) : 0)
     }
-    while (sliderlist.childElementCount > val) {
-      sliderlist.removeChild(sliderlist.lastChild)
+    setSlidersToPreset(preset)
+  }
+  document.getElementById('sawtooth-wave').onclick = e => {
+    let preset = []
+    for (let n = 1; n <= 12; n++) {
+      preset.push(1/n)
     }
+    setSlidersToPreset(preset)
+  }
+  document.getElementById('triangle-wave').onclick = e => {
+    let preset = []
+    let phases = []
+    for (let n = 1; n <= 13; n++) {
+      preset.push(n % 2 ? 8 / ((n * Math.PI) ** 2) : 0)
+      phases.push(n % 4 === 3)
+    }
+    setSlidersToPreset(preset, phases)
   }
   setupPiano()
 }
 
-function midiNumToFrequency(num) {
+function midiNumToFrequency (num) {
   return 13.75 * Math.pow(2, (num - 9) / 12)
 }
 
@@ -48,17 +83,24 @@ function playNote (freq = 440) {
   len = sliderlist.childElementCount
   let real = new Float32Array(len + 1)
   let imag = new Float32Array(len + 1)
-  osc = ctx.createOscillator()
+  const getValue = li => {
+    let el = li.getElementsByClassName('radius')[0]
+    let val = el.value / el.getAttribute('max')
+    if (li.getElementsByClassName('phase')[0].checked) {
+      val *= -1
+    }
+    return val
+  }
 
   real[0] = 0
   imag[0] = 0
 
   for (let i = 0; i < len; i++) {
-    let el = sliderlist.children[i].getElementsByClassName('radius')[0]
-    real[i + 1] = el.value / el.getAttribute('max')
+    real[i + 1] = getValue(sliderlist.children[i])
     imag[i + 1] = 0
   }
 
+  osc = ctx.createOscillator()
   osc.setPeriodicWave(ctx.createPeriodicWave(real, imag))
   osc.frequency.value = freq
   osc.connect(gainNode)
@@ -76,6 +118,9 @@ function draw () {
     let prevx = x
     let prevy = y
     let radius = sliderlist.children[i].getElementsByClassName('radius')[0].value
+    if (sliderlist.children[i].getElementsByClassName('phase')[0].checked) {
+      radius *= -1
+    }
 
     x += radius * cos(time * (i + 1))
     y += radius * sin(time * (i + 1))
@@ -116,6 +161,7 @@ function setupPiano (maxKeys = 25) {
   document.getElementById('volume-slider').onchange = e => {
     gainNode.gain.value = Math.pow(e.target.value, 2)
   }
+  gainNode.gain.value = Math.pow(document.getElementById('volume-slider').value, 2)
 
   const iv = document.getElementById('ivory')
   const eb = document.getElementById('ebony')
